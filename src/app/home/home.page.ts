80786a6c79062b4d10842c205d14a4a01c565136
Trazpadini <luanatrazpadini@hotmail.com>
2019-03-19 16:16:50 -0300

    terceira_versao

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,7 +12,8 @@ export class HomePage {
   valor3 : string = '';
   valor4 :  string = '';
   val1 : number = 0;
-  val2: number =0;
+  val2: number = 0;
+  val3: number = 0;
   clicar():void {
     alert ('PARABÉNS '+this.valor);
   }
@@ -30,8 +31,23 @@ export class HomePage {
   }
   somar (): void {
     let total = Number(this.val1) + Number(this.val2);
-    alert ('A soma é '+total);
+    alert ('A soma é '+ total);
+  }
+  parou (): void {
+    let total1 = Number(this.val1) + Number(this.val2);
+      if ((total1) % 2 ==0){
+      alert ('A soma é par')
+    }
+      else{
+      alert ('A soma é impar')
     }
   }
-}
-}
+    maiorou (): void {
+      if (this.val1 > this.val2){
+        alert ('O primeiro número é o maior. Ele é o '+this.val1);
+      }
+      else {
+        alert ('O segundo número é maior. Ele é o '+this.val2);
+      }
+    }
+  }
