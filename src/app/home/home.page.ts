4a20660dea51f143640a52ea4f9ec325b1b18187
trazpadini <luanatrazpadini@hotmail.com>
2019-04-30 17:11:24 -0300

    quarta_versao

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -14,40 +14,43 @@ export class HomePage {
   val1 : number = 0;
   val2: number = 0;
   val3: number = 0;
+
   clicar():void {
-    alert ('PARABÉNS '+this.valor);
+    alert ('PARABÉNS SEU CONCEITO FOI A '+this.valor);
+
   }
 
   clicar2(): void {
-    alert ('OTIMO MAS DA PRA MELHORAR'+this.valor2);
+    alert ('Parabéns conceito foi B'+this.valor2);
   }
 
   clicar3(): void {
-    alert ('TEM QUE ESTUDAR MAIS'+this.valor3);
+    alert ('Seu conceito foi C'+this.valor3);
   }
 
   clicar4(): void {
-    alert ('VAI ESTUDAR AGORAAAAAAAAAAAAA'+this.valor4);
+    alert ('Seu conceito foi D'+this.valor4);
   }
+
   somar (): void {
     let total = Number(this.val1) + Number(this.val2);
     alert ('A soma é '+ total);
   }
+
   parou (): void {
     let total1 = Number(this.val1) + Number(this.val2);
-      if ((total1) % 2 ==0){
-      alert ('A soma é par')
-    }
-      else{
-      alert ('A soma é impar')
-    }
-  }
-    maiorou (): void {
-      if (this.val1 > this.val2){
-        alert ('O primeiro número é o maior. Ele é o '+this.val1);
-      }
-      else {
-        alert ('O segundo número é maior. Ele é o '+this.val2);
-      }
+    if ((total1) % 2 ==0){
+      alert ('A soma é par');
+    } else{
+      alert ('A soma é impar');
     }
   }
+
+  maiorou (): void {
+    if (Number(this.val1) > Number(this.val2)) {
+      alert ('O primeiro número é o maior.');
+    } else {
+      alert ('O segundo número é maior.');
+    }
+  }
+}
